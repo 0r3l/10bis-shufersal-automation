c6c403ee478bc9938287d30b9e220668e7013f71
Eliyahu, Orel <oe530k@intl.att.com>
2022-04-04 15:36:49 +0300

    bug fix - read file from relative path

--- cookie-parser.js
+++ cookie-parser.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
+const path = require('path')
 
 function parse() {
   var output = [];
-  const cookie = fs.readFileSync('./cookie', { encoding: 'utf8' })
+  const cookie = fs.readFileSync(path.resolve(__dirname, 'cookie'), { encoding: 'utf8' })
   cookie.split(/\s*;\s*/).forEach(function (pair) {
     pair = pair.split(/\s*=\s*/);
     output.push({
@@ -14,7 +15,7 @@ function parse() {
       expires: 1656846315 //Sunday, July 3, 2022 11:05:15 AM
     });
   });
-  console.log(JSON.stringify(output, null, 4))
+  // console.log(JSON.stringify(output, null, 4))
   return output
 }
 
